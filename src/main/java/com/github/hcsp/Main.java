a5a68ef3fdef3b594a014e296da2e3a87f251eb4
dingsheng <dingduoduo0916@gmail.com>
2019-09-22 23:27:34 +0800

    获取新闻标题和链接 重构 1 凡是写注释的地方，都可以考虑重构 2 方法内部实现不要过长 3 一个方法尽量只做一件事情

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -25,51 +25,69 @@ public class Main {
       if (linkPool.isEmpty()) {
         break;
       }
-
       // 移除最后一个
       String link = linkPool.remove(linkPool.size() - 1);
-
       // 如果已经处理过 则跳过
-      if(processedLinks.contains(link)) {
+      if (processedLinks.contains(link)) {
         continue;
       }
-
-      if (link.contains("sina.cn")
-              && !link.contains("passport.sina.cn")
-              && !link.contains("hotnews.sina.cn")
-              && (link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
-        if (link.startsWith("//")) {
-          link = "https:" + link;
-        }
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(link);
-        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-          System.out.println(link);
-          HttpEntity entity1 = response1.getEntity();
-          Document document = Jsoup.parse(EntityUtils.toString(entity1));
-          ArrayList<Element> aTags = document.select("a");
-          for (Element aTag : aTags) {
-            linkPool.add(aTag.attr("href"));
-          }
-
-          // 假如是详情页， 就存入数据库 否则什么都不做
-          ArrayList<Element> articleTags = document.select("article");
-          if (!articleTags.isEmpty()) {
-            for (Element article : articleTags) {
-              System.out.println(article.child(0).text());
-            }
-          }
-
-          // 处理完之后 放到已处理的 set中
-          processedLinks.add(link);
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
+      if (isInterestingLink(link)) {
+        Document document = HttpGetAndParseHtml(link);
+        document.select("a").stream().map(aTag -> linkPool.add(aTag.attr("href")));
+        storeIntoDataBaseIfItIsNewsPage(document);
+        // 处理完之后 放到已处理的 set中
+        processedLinks.add(link);
       } else {
         // 这是我们不感兴趣的 不处理它
       }
     }
 
 
+  }
+
+  private static void storeIntoDataBaseIfItIsNewsPage(Document document) {
+    ArrayList<Element> articleTags = document.select("article");
+    if (!articleTags.isEmpty()) {
+      for (Element article : articleTags) {
+        System.out.println(article.child(0).text());
+      }
+    }
+  }
+
+  private static Document HttpGetAndParseHtml(String link) {
+    if (link.startsWith("//")) {
+      link = "https:" + link;
+    }
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    HttpGet httpGet = new HttpGet(link);
+    try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+      System.out.println(link);
+      HttpEntity entity1 = response1.getEntity();
+      return Jsoup.parse(EntityUtils.toString(entity1));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  /**
+   *  是否为感兴趣的链接
+   * @param link
+   * @return
+   */
+  private static boolean isInterestingLink(String link) {
+    return (isIndexPage(link) || isNewsPage(link)) && !isLoginPage(link);
+  }
+
+  private static boolean isIndexPage(String link) {
+    return "https://sina.cn".equals(link);
+  }
+
+  private static boolean isNewsPage(String link) {
+    return link.contains("news.sina.cn") && !link.contains("hotnews.sina.cn");
+  }
+
+  private static boolean isLoginPage(String link) {
+    return link.contains("passport.sina.cn");
   }
 }
